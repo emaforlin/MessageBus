775de9430adefc1f54df4e7cd4e274a9bddfc8d7
Emanuel Forlin <emaa.forlin@gmail.com>
2025-09-03 09:57:18 -0300

    implements main function

---
+++ main.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/emaforlin/inmembus/internal/core"
+	"github.com/emaforlin/inmembus/internal/server"
+	pb "github.com/emaforlin/inmembus/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterMessageBusServiceServer(grpcServer, server.NewGRPCServer(core.NewInMemoryBus()))
+
+	go func() {
+		log.Println("gRPC server listening on :50051")
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Printf("Shutting down server...")
+	grpcServer.GracefulStop()
+}
